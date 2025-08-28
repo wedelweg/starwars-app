1c6d0a2bc1790b9d7de6d10d82bfc49ea976b5d9
andre <kinosite@gmail.com>
2025-08-28 09:59:38 +0300

    Add all files

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/starwars-app">
             <App />
         </BrowserRouter>
     </StrictMode>
