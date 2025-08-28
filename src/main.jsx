e4c8cfe5af1b9eb51ed4d2d46453a8db29fa9558
andre <kinosite@gmail.com>
2025-08-28 09:21:04 +0300

    Add all files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";   // 👈 вместо HashRouter
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.jsx";
