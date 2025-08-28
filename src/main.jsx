72982c3940d679bc5181c272c0530a9f0c914c91
andre <kinosite@gmail.com>
2025-08-28 09:44:23 +0300

    Add all files

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 // src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; //  BrowserRouter
+import { BrowserRouter } from "react-router-dom"; //
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>
 );
